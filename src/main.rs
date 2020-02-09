53bfa514b5b702f53141e75611b38ffcc8bd6386
kaveh <kaveh.ka@gmail.com>
2020-02-09 15:33:41 -0600

    Include README.md contents in the docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+//! # rust-mbtileserver
+//!
+//! A simple Rust-based server for map tiles stored in mbtiles format.
+//!
+//! ## Usage
+//!
+//! Run `mbtileserver --help` for a list and description of the available flags:
+//!
+//! ```
+//! MBTiles Server
+//!
+//! USAGE:
+//!     mbtileserver [OPTIONS]
+//!
+//! FLAGS:
+//!     -h, --help       Prints help information
+//!     -V, --version    Prints version information
+//!
+//! OPTIONS:
+//!     -d, --directory <directory>    Tiles directory [default: ./tiles]
+//!     -p, --port <port>              Port [default: 3000]
+//! ```
+//!
+//! Run `mbtileserver` to start serving the mbtiles in a given folder. The default folder is `./tiles` and you can change it with `-d` flag.
+//! The server starts on port 3000 by default. You can use a different port via `-p` flag.
+//!
+//! ### Endpoints
+//!
+//! | Endpoint                                                    | Description                                                                    |
+//! |-------------------------------------------------------------|--------------------------------------------------------------------------------|
+//! | /services                                                   | lists all discovered and valid mbtiles in the tiles directory                  |
+//! | /services/<path-to-tileset>                                 | shows tileset metadata                                                         |
+//! | /services/<path-to-tileset>/map                             | tileset preview                                                                |
+//! | /services/<path-to-tileset>/tiles/{z}/{x}/{y}.<tile-format> | returns tileset tile at the given x, y, and z                                  |
+//! | /services/<path-to-tileset>/tiles/{z}/{x}/{y}.json          | returns UTFGrid data at the given x, y, and z (only for tilesets with UTFGrid) |
+
 extern crate clap;
 extern crate flate2;
 extern crate hyper;
